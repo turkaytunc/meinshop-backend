2477a23cca6fad8c924ccd631a1c91c5bb4200c2
Turkay Tunc <turkaytunc00@gmail.com>
2020-12-06 12:05:25 +0300

    add: products and single product get endpoints

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const app = express();
+const fetch = require('node-fetch');
 
 app.get('/', (req, res) => {
   res.send('API is working!');
+});
+
+app.get('/api/products', (req, res) => {
+  fetch('https://fakestoreapi.com/products')
+    .then((data) => data.json())
+    .then((parsedData) => res.json(parsedData));
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const id = parseInt(req.params.id) || null;
+
+  if (id != null) {
+    return fetch(`https://fakestoreapi.com/products/${id}`)
+      .then((data) => data.json())
+      .then((parsedData) => {
+        if (parsedData) {
+          res.json(parsedData);
+        } else {
+          res.status(404).send('Cant find item');
+        }
+      });
+  }
+  return res.status(500).send('Server Error');
 });
 
 app.listen(5000, () => {
