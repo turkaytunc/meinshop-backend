af9d1b7832696b0819a60023301621b6ce917bbe
Turkay Tunc <turkaytunc00@gmail.com>
2020-12-06 12:12:30 +0300

    fix: cross origin error

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('API is working!');
 });
