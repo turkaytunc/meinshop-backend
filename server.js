54189424544832659d0fc3699782f002e03406a4
Turkay Tunc <turkaytunc00@gmail.com>
2020-12-06 11:17:11 +0300

    add: initial setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
+
+app.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
+});
